0fb0fff957e9ec381651f991a94a35f38a046eda
fredericradigoy <dev@frederic-radigoy.fr>
2024-03-02 22:17:25 +0100

    feat: création du test permetant de faire une serie de 1 point par lancer

--- kata-bowling/src/test/java/fr/fradigoy/BowlingTest.java
+++ kata-bowling/src/test/java/fr/fradigoy/BowlingTest.java
@@ -26,6 +26,14 @@ public class BowlingTest {
         assertThat(0).isEqualTo(g.score());
     }
 
+    @Test
+    void allOnes () {
+        for (int i = 0; i < 20; i++) {
+            g.roll(1);
+        }
+        assertThat(20).isEqualTo(g.score());
+    }
+
 
 
 }
