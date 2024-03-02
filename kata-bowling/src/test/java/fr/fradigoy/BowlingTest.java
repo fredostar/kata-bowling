0f55f2f569cb5ab805b217aba0ccf3068f7040a5
fredericradigoy <dev@frederic-radigoy.fr>
2024-03-02 21:56:46 +0100

    feat: création du tests

--- kata-bowling/src/test/java/fr/fradigoy/BowlingTest.java
+++ kata-bowling/src/test/java/fr/fradigoy/BowlingTest.java
@@ -18,7 +18,13 @@ public class BowlingTest {
         g.roll(0);
     }
 
-
+    @Test
+    void gutterGame() {
+        for (int i = 0; i < 20; i++) {
+            g.roll(0);
+        }
+        assertThat(0).isEqualTo(g.score());
+    }
 
 
 
